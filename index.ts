2e3972841c058f95f6ed7ed2c28b5db974ff8d51
kevy60 <kevin.vorsmann@voco.ee>
2024-03-12 09:18:11 +0200

    Mudelite valiku täiendamine

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 import articleController from "./controllers/articleContoller";
+import commentController from "./controllers/commentController";
 import bodyParser from 'body-parser';
 
 mongoose.connect("mongodb+srv://kevinvorsmann:qwerty1@cluster0.d40x17p.mongodb.net/");
@@ -11,14 +12,14 @@ const app: Express = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Define your routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
 app.use('/', articleController);
+app.use('/', commentController);
 
-// Database connection handling
+
 database.on('error', (error) => {
     console.log(error)
 });
@@ -27,7 +28,6 @@ database.once('connected', () => {
     console.log('Database Connected');
 });
 
-// Start the server
 app.listen(3000,() => {
   console.log(`[server]: Server is running at http://localhost:3000`);
 });
